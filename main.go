04fabddeb1b8a8f9d9da607cb297301a9e3e1b8c
Himanshu <himanshurnjn04@gmail.com>
2019-12-20 18:24:45 +0530

    Just getting started

--- main.go
+++ main.go
@@ -12,57 +12,70 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type buildInfo struct {
-	buildId int
-	status bool
+type jobInfo struct {
+	Id int
+	Status bool
 }
 
+var (
+	builds = 100000
+	tests = 100
+)
+
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	builds, _ := strconv.Atoi(scanner.Text())
 
 	var wg sync.WaitGroup
 
-	var ch = make(chan bool, builds)
+	var buildsCh = make(chan jobInfo, builds)
+	var testersCh = make(chan jobInfo, tests)
 
 	for i := 0; i < builds; i++ {
 		wg.Add(1)
-		newBuild := buildInfo{buildId: i, status: false}
-		go builder(&ch, newBuild)
+		go buildRunner(&buildsCh, i, &wg)
 	}
 
-	for i := range ch {
-
+	for i := 0; i < tests; i++ {
+		wg.Add(1)
+		go testRunner(&testersCh, &wg)
 	}
-	close(ch)
+
+	for i := range buildsCh {
+		if i.Status {
+			testersCh <- jobInfo{Id:i.Id, Status:false}
+		}
+	}
+	close(buildsCh)
+	wg.Wait()
 }
 
-func builder(buildChan *chan bool, buildInf buildInfo) {
-	fmt.Println("...Running Build..." + strconv.Itoa(buildInf.buildId))
+func buildRunner(buildChan *chan jobInfo, id int, wg *sync.WaitGroup) {
+	fmt.Println("...Running Build..." + strconv.Itoa(id))
 	time.Sleep(time.Second * time.Duration(rand.Intn(10)))
 	status := rand.Float64() * 1
 	if status < 0.5 {
-		log.Info("Job #" + strconv.Itoa(buildInf.buildId) + ": BUILD FAILED!")
-		*buildChan <- false
+		log.Info("Job #" + strconv.Itoa(id) + ": BUILD FAILED!")
+		*buildChan <- jobInfo{Id:id, Status:false}
 	} else {
-		log.Info("Job #" + strconv.Itoa(buildInf.buildId) + ": BUILD PASSED!")
-		*buildChan <- true
-		go test()
+		log.Info("Job #" + strconv.Itoa(id) + ": BUILD PASSED!")
+		*buildChan <- jobInfo{Id:id, Status:true}
 	}
+	wg.Done()
 }
 
-func test() {
-	fmt.Println("...Running Build..." + strconv.Itoa(buildInf.buildId))
-	time.Sleep(time.Second * time.Duration(rand.Intn(10)))
-	status := rand.Float64() * 1
-	if status < 0.5 {
-		log.Info("Job #" + strconv.Itoa(buildInf.buildId) + ": BUILD FAILED!")
-		*buildChan <- false
-	} else {
-		log.Info("Job #" + strconv.Itoa(buildInf.buildId) + ": BUILD PASSED!")
-		*buildChan <- false
+func testRunner(testerChan *chan jobInfo, wg *sync.WaitGroup) {
+	for i := range *testerChan {
+		fmt.Println("...Running Tests..." + strconv.Itoa(i.Id))
+		time.Sleep(time.Second * time.Duration(rand.Intn(10)))
+		status := rand.Float64() * 1
+		if status < 0.5 {
+			log.Info("Job #" + strconv.Itoa(i.Id) + ": BUILD FAILED!")
+			*buildChan <- false
+		} else {
+			log.Info("Job #" + strconv.Itoa(buildInf.buildId) + ": BUILD PASSED!")
+			*buildChan <- false
+		}
 	}
+	wg.Done()
 }
 
 func deploy() {
